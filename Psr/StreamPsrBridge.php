23df24ae6f4dba59c117141f575fed04059d016e
Payam Naderi <naderi.payam@gmail.com>
2016-05-15 13:01:22 +0430

    implement poirot streamable to psr bridge

---
+++ Psr/StreamPsrBridge.php
@@ -0,0 +1,234 @@
+<?php
+namespace Poirot\vendor\poirot\stream\Poirot\Stream\Psr;
+
+use Poirot\Stream\Interfaces\iStreamable;
+use Psr\Http\Message\StreamInterface;
+
+class StreamPsrBridge 
+    implements StreamInterface
+{
+    /** @var iStreamable */
+    protected $streamable;
+    
+    
+    /**
+     * StreamPsrBridge constructor.
+     * @param iStreamable $streamable
+     */
+    function __construct(iStreamable $streamable)
+    {
+        $this->streamable = $streamable;
+    }
+
+    /**
+     * Reads all data from the stream into a string, from the beginning to end.
+     *
+     * This method MUST attempt to seek to the beginning of the stream before
+     * reading data and read the stream until the end is reached.
+     *
+     * Warning: This could attempt to load a large amount of data into memory.
+     *
+     * This method MUST NOT raise an exception in order to conform with PHP's
+     * string casting operations.
+     *
+     * @see http://php.net/manual/en/language.oop5.magic.php#object.tostring
+     * @return string
+     */
+    public function __toString()
+    {
+        $streamable = $this->streamable;
+        
+        try 
+        {
+            (!$streamable->resource()->isSeekable()) ?: $streamable->seek(0);
+            return $this->getContents();
+        } catch (\Exception $e) {
+            trigger_error($e->getMessage(), E_USER_ERROR);
+            return '';
+        }
+    }
+
+    /**
+     * Closes the stream and any underlying resources.
+     *
+     * @return void
+     */
+    public function close()
+    {
+        $this->streamable->resource()->close();
+    }
+
+    /**
+     * Separates any underlying resources from the stream.
+     *
+     * After the stream has been detached, the stream is in an unusable state.
+     *
+     * @return resource|null Underlying PHP stream, if any
+     */
+    public function detach()
+    {
+        $this->close();
+        $this->streamable = null;
+    }
+
+    /**
+     * Get the size of the stream if known.
+     *
+     * @return int|null Returns the size in bytes if known, or null if unknown.
+     */
+    public function getSize()
+    {
+        return $this->streamable->getSize();
+    }
+
+    /**
+     * Returns the current position of the file read/write pointer
+     *
+     * @return int Position of the file pointer
+     * @throws \RuntimeException on error.
+     */
+    public function tell()
+    {
+        return $this->streamable->getCurrOffset();
+    }
+
+    /**
+     * Returns true if the stream is at the end of the stream.
+     *
+     * @return bool
+     */
+    public function eof()
+    {
+        return $this->streamable->isEOF();
+    }
+
+    /**
+     * Returns whether or not the stream is seekable.
+     *
+     * @return bool
+     */
+    public function isSeekable()
+    {
+        return $this->streamable->resource()->isSeekable();
+    }
+
+    /**
+     * Seek to a position in the stream.
+     *
+     * @link http://www.php.net/manual/en/function.fseek.php
+     * @param int $offset Stream offset
+     * @param int $whence Specifies how the cursor position will be calculated
+     *     based on the seek offset. Valid values are identical to the built-in
+     *     PHP $whence values for `fseek()`.  SEEK_SET: Set position equal to
+     *     offset bytes SEEK_CUR: Set position to current location plus offset
+     *     SEEK_END: Set position to end-of-stream plus offset.
+     * @throws \RuntimeException on failure.
+     */
+    public function seek($offset, $whence = SEEK_SET)
+    {
+        $this->streamable->seek($offset, $whence);
+    }
+
+    /**
+     * Seek to the beginning of the stream.
+     *
+     * If the stream is not seekable, this method will raise an exception;
+     * otherwise, it will perform a seek(0).
+     *
+     * @see seek()
+     * @link http://www.php.net/manual/en/function.fseek.php
+     * @throws \RuntimeException on failure.
+     */
+    public function rewind()
+    {
+        $this->streamable->rewind();
+    }
+
+    /**
+     * Returns whether or not the stream is writable.
+     *
+     * @return bool
+     */
+    public function isWritable()
+    {
+        return $this->streamable->resource()->isWritable();
+    }
+
+    /**
+     * Write data to the stream.
+     *
+     * @param string $string The string that is to be written.
+     * @return int Returns the number of bytes written to the stream.
+     * @throws \RuntimeException on failure.
+     */
+    public function write($string)
+    {
+        $this->streamable->write($string);
+        return $this->streamable->getTransCount();
+    }
+
+    /**
+     * Returns whether or not the stream is readable.
+     *
+     * @return bool
+     */
+    public function isReadable()
+    {
+        return $this->streamable->resource()->isReadable();
+    }
+
+    /**
+     * Read data from the stream.
+     *
+     * @param int $length Read up to $length bytes from the object and return
+     *     them. Fewer than $length bytes may be returned if underlying stream
+     *     call returns fewer bytes.
+     * @return string Returns the data read from the stream, or an empty string
+     *     if no bytes are available.
+     * @throws \RuntimeException if an error occurs.
+     */
+    public function read($length)
+    {
+        return $this->streamable->read($length);
+    }
+
+    /**
+     * Returns the remaining contents in a string
+     *
+     * @return string
+     * @throws \RuntimeException if unable to read or an error occurs while
+     *     reading.
+     */
+    public function getContents()
+    {
+        $content = '';
+        while(!$this->streamable->isEOF()) {
+            $r = $this->streamable->read(1048576);
+            if ($r == null)
+                break;
+            $content .= $r;
+        }
+        
+        return $content;
+    }
+
+    /**
+     * Get stream metadata as an associative array or retrieve a specific key.
+     *
+     * The keys returned are identical to the keys returned from PHP's
+     * stream_get_meta_data() function.
+     *
+     * @link http://php.net/manual/en/function.stream-get-meta-data.php
+     * @param string $key Specific metadata to retrieve.
+     * @return array|mixed|null Returns an associative array if no key is
+     *     provided. Returns a specific key value if a key is provided and the
+     *     value is found, or null if the key is not found.
+     */
+    public function getMetadata($key = null)
+    {
+        if ($key !== null)
+            return $this->streamable->resource()->meta()->getMetaKey($key);
+        
+        return $this->streamable->resource()->meta()->toArray();
+    }
+}
