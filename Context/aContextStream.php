91dd7efaefae5500c424420e8e21e5b5156fa361
Payam Naderi <payam.naderi@westwing.de>
2021-08-30 19:57:45 +0200

    code cleanup

--- Context/aContextStream.php
+++ Context/aContextStream.php
@@ -4,8 +4,8 @@ namespace Poirot\Stream\Context;
 // DO_LEAST_PHPVER_SUPPORT 5.5 yield
 if (version_compare(phpversion(), '5.5.0') < 0) {
     ## php version not support traits
-    require_once __DIR__.'/fixes/aContextStream.php';
+    require_once __DIR__ . '/fixes/aContextStream.php';
     return;
 }
 
-require_once __DIR__.'/aContextStream.fix.php';
+require_once __DIR__ . '/aContextStream.fix.php';
